8b7e6494cd6c31d2b8121891b3365d54e3a81e64
andrejs82 <andrejs82@gmail.com>
2017-11-01 17:04:21 -0300

    #4 created main logic

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#BrainGames on HexletProject
+### BrainGames on HexletProject
 
 <a href="https://codeclimate.com/github/andrejs82git/project-lvl1-s204/maintainability"><img src="https://api.codeclimate.com/v1/badges/8e9ccba3ea1e500af3b2/maintainability" /></a>
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,41 @@
 import readlineSync from 'readline-sync';
 
+const ANSWER_IS_EVEN = 'yes';
+const ANSWER_IS_NOT_EVEN = 'no';
+
+const getRandom = () =>
+  Math.floor(Math.random() * 100);
+
+
+const isEvenNumber = num =>
+  num % 2 === 0;
+
+
+const getCorrectAnswer = (num) => {
+  const isEven = isEvenNumber(num);
+  return isEven ? ANSWER_IS_EVEN : ANSWER_IS_NOT_EVEN;
+};
+
 const run = () => {
+  let countCorrectAnswers = 0;
+
   console.log('Welcome to the Brain Games!\n');
   const userName = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${userName}!`);
+
+  while (countCorrectAnswers < 3) {
+    const randomNumber = getRandom();
+    console.log(`Question:  ${randomNumber}`);
+    const answer = readlineSync.question('Your answer (yes/no): ');
+    const correctAnswer = getCorrectAnswer(randomNumber);
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+      countCorrectAnswers += 1;
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export default run;
