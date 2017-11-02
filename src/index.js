4952950a62fc195e1c71204fc950866bd35cfba9
andrejs82 <andrejs82@gmail.com>
2017-11-02 15:18:22 -0300

    #5 add new game Calc + refactoring

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import readlineSync from 'readline-sync';
 
-const ANSWER_IS_EVEN = 'yes';
-const ANSWER_IS_NOT_EVEN = 'no';
-
 export const greeting = () => {
   console.log('Welcome to the Brain Games!\n');
   const userName = readlineSync.question('May I have your name?: ');
@@ -10,30 +7,21 @@ export const greeting = () => {
   return userName;
 };
 
-export const runBrainGames = () => {
-  greeting();
-};
+const getRandom = capacity => Math.floor(Math.random() * capacity);
 
-const getRandom = () =>
-  Math.floor(Math.random() * 100);
+const getQuestion = task => task('question');
 
-const isEvenNumber = num =>
-  num % 2 === 0;
+const getAnswer = task => task('answer');
 
-const getCorrectAnswer = (num) => {
-  const isEven = isEvenNumber(num);
-  return isEven ? ANSWER_IS_EVEN : ANSWER_IS_NOT_EVEN;
-};
-
-export const runBrainEven = () => {
+const runMainFlow = (taskGen) => {
   let countCorrectAnswers = 0;
   const userName = greeting();
 
   while (countCorrectAnswers < 3) {
-    const randomNumber = getRandom();
-    console.log(`Question:  ${randomNumber}`);
-    const answer = readlineSync.question('Your answer (yes/no): ');
-    const correctAnswer = getCorrectAnswer(randomNumber);
+    const task = taskGen();
+    console.log(`Question:  ${getQuestion(task)}`);
+    const answer = readlineSync.question('Your answer: ');
+    const correctAnswer = getAnswer(task);
     if (answer === correctAnswer) {
       console.log('Correct!');
       countCorrectAnswers += 1;
@@ -42,5 +30,62 @@ export const runBrainEven = () => {
     }
   }
   console.log(`Congratulations, ${userName}!`);
+};
+
+const taskEvenGen = () => {
+  const ANSWER_IS_EVEN = 'yes';
+  const ANSWER_IS_NOT_EVEN = 'no';
+  const randomNumber = getRandom(100);
+  const isEven = (randomNumber % 2) === 0;
+  const correctAnswer = isEven ? ANSWER_IS_EVEN : ANSWER_IS_NOT_EVEN;
+
+  return (prop) => {
+    switch (prop) {
+      case 'question': {
+        return randomNumber;
+      }
+      case 'answer': {
+        return correctAnswer;
+      }
+      default: return null;
+    }
+  };
+};
+
+const taskCalcGen = () => {
+  const operations = [
+    { name: '+', answer: (a, b) => a + b },
+    { name: '-', answer: (a, b) => a - b },
+    { name: '*', answer: (a, b) => a * b },
+  ];
+  const op = operations[getRandom(3)];
+  const num1 = getRandom(10);
+  const num2 = getRandom(10);
+
+  const correctAnswer = op.answer(num1, num2);
+
+  return (prop) => {
+    switch (prop) {
+      case 'question': {
+        return `${num1} ${op.name} ${num2}`;
+      }
+      case 'answer': {
+        return `${correctAnswer}`;
+      }
+      default: return null;
+    }
+  };
+};
+
+export const runBrainGames = () => {
+  greeting();
+};
+
+export const runBrainEven = () => {
+  runMainFlow(taskEvenGen);
+};
+
+export const runBrainCalc = () => {
+  runMainFlow(taskCalcGen);
 };
 
