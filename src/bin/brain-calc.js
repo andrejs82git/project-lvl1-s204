edb5a60936cd5d4eb75caf3581135c2b6777f412
andrejs82 <andrejs82@gmail.com>
2017-11-03 11:24:48 -0300

    refactoring to modules

--- src/bin/brain-calc.js
+++ src/bin/brain-calc.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import { runBrainCalc } from '..';
+import run from '../games/game-calc.js';
 
-runBrainCalc();
+run();
---
+++ src/games/game-calc.js
@@ -0,0 +1,32 @@
+import { runMainFlow } from '../index.js';
+import { getRandom } from '../utils.js';
+
+const taskCalcGen = () => {
+  const operations = [
+    { name: '+', answer: (a, b) => a + b },
+    { name: '-', answer: (a, b) => a - b },
+    { name: '*', answer: (a, b) => a * b },
+  ];
+  const op = operations[getRandom(0, 2)];
+  const num1 = getRandom(0, 10);
+  const num2 = getRandom(0, 10);
+
+  const correctAnswer = op.answer(num1, num2);
+
+  return (prop) => {
+    switch (prop) {
+      case 'question': {
+        return `${num1} ${op.name} ${num2}`;
+      }
+      case 'answer': {
+        return `${correctAnswer}`;
+      }
+      default: return null;
+    }
+  };
+};
+
+export default () => {
+  runMainFlow(taskCalcGen);
+};
+
