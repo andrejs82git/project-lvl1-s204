103502fc79e84b934faed332ff861f2abf3db197
andrejs82 <andrejs82@gmail.com>
2017-11-04 16:22:31 -0300

    #7 fix logic problem

--- src/games/game-balance.js
+++ src/games/game-balance.js
@@ -12,16 +12,18 @@ const isBalance = (nums) => {
   return true;
 };
 
+const getMax = array =>
+  array.reduce((iMax, n, index, arr) => (n > arr[iMax] ? index : iMax), 0);
+
+const getMin = array =>
+  array.reduce((iMin, n, index, arr) => (n < arr[iMin] ? index : iMin), 0);
+
 const bitBalance = (pnums) => {
   const nums = pnums;
-  for (let i = 0; i < nums.length - 1; i += 1) {
-    const n1 = nums[i];
-    const n2 = nums[i + 1];
-    if ((n2 - n1) > 1) {
-      nums[i] += 1;
-      nums[i + 1] -= 1;
-    }
-  }
+  const indexMax = getMax(nums);
+  const indexMin = getMin(nums);
+  nums[indexMax] -= 1;
+  nums[indexMin] += 1;
 };
 
 const taskBalanceGen = () => {
@@ -33,9 +35,9 @@ const taskBalanceGen = () => {
     .sort();
   while (!isBalance(nums)) {
     bitBalance(nums);
-    nums = nums.sort();
   }
 
+  nums = nums.sort();
   const ans = nums.reduce((acc, i) => acc + i, '');
   return {
     question: `${num}`,
