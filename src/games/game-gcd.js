e8378aaf270a988b69dc5f6d1fee557b000e4de1
andrejs82 <andrejs82@gmail.com>
2017-11-04 12:44:03 -0300

    move func to upper level

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -5,15 +5,6 @@ const taskGcdGen = () => {
   const num1 = getRandom(0, 100);
   const num2 = getRandom(0, 100);
 
-  const minDivide = (a, b) => {
-    const min = a < b ? a : b;
-    let result = 1;
-    for (let q = 1; q <= min; q += 1) {
-      if (a % q === 0 && b % q === 0) result = q;
-    }
-    return result;
-  };
-
   const correctAnswer = minDivide(num1, num2);
 
   return (prop) => {
@@ -27,6 +18,15 @@ const taskGcdGen = () => {
       default: return null;
     }
   };
+};
+
+const minDivide = (a, b) => {
+  const min = a < b ? a : b;
+  let result = 1;
+  for (let q = 1; q <= min; q += 1) {
+    if (a % q === 0 && b % q === 0) result = q;
+  }
+  return result;
 };
 
 export default () => {
