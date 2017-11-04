7ee68571072dc06781d89e0e68ceb91c6af374a4
andrejs82 <andrejs82@gmail.com>
2017-11-04 16:47:40 -0300

    #7 fix logic problem

--- src/games/game-balance.js
+++ src/games/game-balance.js
@@ -1,27 +1,19 @@
 import { runMainFlow } from '..';
 import { getRandom } from '../utils';
 
-const isBalance = (nums) => {
-  for (let i = 0; i < nums.length - 1; i += 1) {
-    const n1 = nums[i];
-    const n2 = nums[i + 1];
-    if ((n2 - n1) > 1) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const getMax = array =>
+const getMaxIndex = array =>
   array.reduce((iMax, n, index, arr) => (n > arr[iMax] ? index : iMax), 0);
 
-const getMin = array =>
+const getMinIndex = array =>
   array.reduce((iMin, n, index, arr) => (n < arr[iMin] ? index : iMin), 0);
+
+const isBalance = nums =>
+  (nums[getMaxIndex(nums)] - nums[getMinIndex(nums)]) <= 1;
 
 const bitBalance = (pnums) => {
   const nums = pnums;
-  const indexMax = getMax(nums);
-  const indexMin = getMin(nums);
+  const indexMax = getMaxIndex(nums);
+  const indexMin = getMinIndex(nums);
   nums[indexMax] -= 1;
   nums[indexMin] += 1;
 };
