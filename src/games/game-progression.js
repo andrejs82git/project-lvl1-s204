254eeb51bde3640c7ef1bf8d527804755793eee7
andrejs82 <andrejs82@gmail.com>
2017-11-05 10:56:41 -0300

    refactoring and fix linet problem with utils

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -1,17 +1,8 @@
 import { runMainFlow } from '..';
-import { getRandom } from '../utils';
-
-const progressGenBuilder = (first, step) => {
-  let next = first;
-  return () => {
-    const result = next;
-    next += step;
-    return result;
-  };
-};
+import { getRandom, progressArifmGenBuilder } from '../utils';
 
 const taskProgressionGen = () => {
-  const progressGen = progressGenBuilder(getRandom(0, 20), getRandom(1, 20));
+  const progressGen = progressArifmGenBuilder(getRandom(0, 20), getRandom(1, 20));
   const progression = Array.from({ length: 10 }, () => progressGen());
   const qIndex = getRandom(0, 9);
   const answer = progression[qIndex];
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,13 @@
 const getRandom = (min, max) =>
   Math.floor(Math.random() * ((max - min) + 1)) + min;
 
-export { getRandom as default, getRandom };
+const progressArifmGenBuilder = (first, step) => {
+  let next = first;
+  return () => {
+    const result = next;
+    next += step;
+    return result;
+  };
+};
+
+export { progressArifmGenBuilder, getRandom };
